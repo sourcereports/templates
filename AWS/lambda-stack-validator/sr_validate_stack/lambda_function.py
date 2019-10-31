0e818caa85fbed9229989ea34717aee077a2c95e
Andrew Kondelin <akondelin@Andrews-MacBook-Pro-2.local>
2019-10-31 18:52:40 +0700

    updated stack validator

--- AWS/lambda-stack-validator/sr_validate_stack/lambda_function.py
+++ AWS/lambda-stack-validator/sr_validate_stack/lambda_function.py
@@ -4,39 +4,61 @@ import socket
 import dns.resolver
 import base64
 import time
+from datetime import datetime
+from decimal import Decimal
 from boto3.dynamodb.conditions import Key, Attr
 
 class Stack:
 
     # Initializer / Instance Attributes
     def __init__(self, response):
-        self.url = response['Items'][0]['url']
-        self.secondurl = response['Items'][0]['secondurl']
-        self.hostid = response['Items'][0]['hostid']
-        self.rolearn = response['Items'][0]['rolearn']
-        self.id = response['Items'][0]['id']
-        self.codepipelineid = response['Items'][0]['codepipelineid']
-        self.loadbalancerarn = response['Items'][0]['loadbalancerarn']
+        print(response)
+        self.url = response['url']
+        self.secondurl = response['secondurl']
+        self.hostid = response['hostid']
+        self.rolearn = response['rolearn']
+        self.id = response['id']
+        self.codepipelineid = response['codepipelineid']
+        self.loadbalancerarn = response['loadbalancerarn']
         self.credentials = AssumeRole(self.rolearn)
         self.dnscount = 0
+        self.flag = 0
         self.redirect_bucket = ''
         self.msg = ''
         self.new = ''
         try:
-            self.repo = response['Items'][0]['repo']
-            self.branch = response['Items'][0]['branch']
+            self.repo = response['repo']
+            self.branch = response['branch']
         except:
             self.repo = ''
             self.branch = ''
+
+def getRecord(url):
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+    timestamp = Decimal(timestamp)
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('sourcereports')
+
+    response = table.scan(
+        FilterExpression = Attr('type').eq('stack') & Attr("url").eq(url)
+    )
+    items = response['Items']
+    recent = max(items,key=lambda item:item['timestamp'])
+    print (recent)
+
+    return recent
 
 
 def lambda_handler(event, context):
 
     myvar = json.loads(event['body'])
     url = myvar['url']
+    print('hi')
+    print(url)
 
-    id = getId(url)
-    response = getRecord(id)
+    response = getRecord(url)
     my_stack = Stack(response)
     getLiveNS(my_stack)
     pipelineInfo(my_stack)
@@ -56,9 +78,13 @@ def lambda_handler(event, context):
 
     if my_stack.deploymentInstances != my_stack.live_targets:
         my_stack.msg ="Instances not identical"
+        print('msg: "Instances not identical"')
+        print(my_stack.deploymentInstances)
+        print(my_stack.live_targets)
 
 
     getASGinfo(my_stack)
+    deploymentVsASG(my_stack)
 
     if my_stack.msg is '':
         my_stack.msg = 'pass'
@@ -79,21 +105,6 @@ def lambda_handler(event, context):
         },
         'body': json.dumps({"msg":my_stack.msg, "repo":my_stack.repo, "uid":my_stack.id})
     }
-def getId(url):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('sourceshare')
-    response = table.query(
-      KeyConditionExpression=Key('id').eq(id)
-    )
-    return response
-
-def getRecord(id):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('sourceshare')
-    response = table.query(
-      KeyConditionExpression=Key('id').eq(id)
-    )
-    return response
 
 
 def AssumeRole(rolearn):
@@ -158,9 +169,23 @@ def pipelineInfo(my_stack):
         aws_secret_access_key=my_stack.credentials['SECRET_KEY'],
         aws_session_token=my_stack.credentials['SESSION_TOKEN']
     )
+    response = client.get_pipeline_state(
+        name=my_stack.codepipelineid
+    )
+    print(response)
+    for pipeline_state in response['stageStates']:
+        if pipeline_state['stageName'] == 'Deploy':
+            for action_state in pipeline_state['actionStates']:
+                if action_state['actionName'] == 'Deploy':
+                    if action_state['latestExecution']['status'] == 'Succeeded':
+                        my_stack.latest_execution = action_state['latestExecution']['externalExecutionId']
+                        print(my_stack.latest_execution)
+                    else:
+                        my_stack.msg = 'This site is currently undergoing a new deployment, please wait a few minutes.'
     response = client.get_pipeline(
         name=my_stack.codepipelineid
     )
+    print(response)
     for stage in response['pipeline']['stages']:
         print(stage)
         if stage['name'] == 'Source':
@@ -186,7 +211,7 @@ def pipelineInfo(my_stack):
                 if action['actionTypeId']['provider'] == 'CodeDeploy':
                     my_stack.codedeployApp = action['configuration']['ApplicationName']
                     my_stack.deploymentgroupName = action['configuration']['DeploymentGroupName']
-                    print (my_stack.deploymentgroupName)
+                    print ('deploy: '+my_stack.deploymentgroupName)
 
 def codedeployInfo(my_stack):
     client = boto3.client('codedeploy',
@@ -198,17 +223,103 @@ def codedeployInfo(my_stack):
         applicationName=my_stack.codedeployApp,
         deploymentGroupName=my_stack.deploymentgroupName
     )
-    deploymentid = response['deploymentGroupInfo']['lastSuccessfulDeployment']['deploymentId']
+    my_stack.deploymentid = response['deploymentGroupInfo']['lastSuccessfulDeployment']['deploymentId']
+
     response = client.list_deployment_instances(
-        deploymentId=deploymentid
+        deploymentId=my_stack.deploymentid
     )
     instanceids = response['instancesList']
-    response = client.batch_get_deployment_instances(
-        deploymentId=deploymentid,
-        instanceIds=instanceids
-    )
-    #save in case I need ec2 ARNs
     my_stack.deploymentInstances = set(instanceids)
+
+def deploymentVsASG(my_stack):
+
+
+    risky_deployments=[]
+    new_deployments=[]
+    deployment_info = []
+    deployments_data = []
+    last_success= 0
+    good_deployments = 0
+    bad_deployments = 0
+
+
+    # response = client.list_deployment_instances(
+    #     deploymentId=deploymentid
+    # )
+    # instanceids = response['instancesList']
+    client = boto3.client('codedeploy',
+        aws_access_key_id=my_stack.credentials['ACCESS_KEY'],
+        aws_secret_access_key=my_stack.credentials['SECRET_KEY'],
+        aws_session_token=my_stack.credentials['SESSION_TOKEN']
+    )
+
+    response = client.list_applications()
+
+    for application in response['applications'] :
+        response = client.list_deployment_groups(applicationName = application)
+        try:
+            response = client.batch_get_deployment_groups(
+                applicationName=response['applicationName'],
+                deploymentGroupNames=response['deploymentGroups']
+            )
+
+            for deploymentgroup in response['deploymentGroupsInfo']:
+
+                print(deploymentgroup)
+
+                #no tag based filter codedeploy actions allowed
+                if 'ec2TagSet' in deploymentgroup:
+                    if not deploymentgroup['ec2TagSet']['ec2TagSetList']:
+                        pass
+                    else:
+                        my_stack.msg = 'Ec2-tag codedeploy deployments not allowed. Account flagged.'
+                        bad_deployments = bad_deployments + 1
+                        my_stack.flag = 1
+
+                #no tag based filter codedeploy actions allowed
+                elif 'ec2TagFilters' in deploymentgroup:
+                    if not deploymentgroup['ec2TagFilters']:
+                        pass
+                    else:
+                        my_stack.msg = 'Ec2-filter codedeploy deployments not allowed. Account flagged.'
+                        my_stack.flag=2
+                        bad_deployments = bad_deployments + 1
+
+                if 'autoScalingGroups' in deploymentgroup:
+                    my_stack.codedeploy_asg = deploymentgroup['autoScalingGroups'][0]['name']
+                    my_stack.codedeploy_deployment_group = deploymentgroup['deploymentGroupName']
+
+                    if my_stack.codedeploy_asg == my_stack.ASG:
+                        if my_stack.deploymentgroupName == my_stack.codedeploy_deployment_group:
+                            if len(deploymentgroup['autoScalingGroups']) > 1:
+                                my_stack.msg = 'Only 1 ASG allowed; the one defined in the pipeline'
+                            else:
+                                if my_stack.deploymentid == my_stack.latest_execution:
+                                    good_deployments = good_deployments + 1
+                                else:
+                                    my_stack.msg = "Your pipeline deployment has been overwritten by a different deployment, so you've been flagged."
+                                    my_stack.flag = 3
+                        else:
+                            #flag for mischief
+                            bad_deployments = bad_deployments + 1
+                            my_stack.msg = 'You cannot deploy anything except your CodePipeline deployment group to your AutoScalingGroup'
+                            my_stack.flag = 4
+                    else:
+                        pass
+
+
+                else:
+                    pass
+
+        except:
+            print('could not locate deployment groups:')
+    print('deployments: '+str(good_deployments) +' good '+ str(bad_deployments) + ' bad')
+    if good_deployments<1:
+        my_stack.msg = "Your pipeline is not working because your pipeline CodeDeploy is not reflecting your AutoScalingGroup's current deployment"
+
+
+    #save in case I need ec2 ARNs
+
 
 def getLBinfo(my_stack):
     client = boto3.client('elbv2',
@@ -236,16 +347,16 @@ def getLBinfo(my_stack):
             if listener['DefaultActions'][0]['Type'] == 'forward':
                 my_stack.target_group = listener['DefaultActions'][0]['TargetGroupArn']
             else:
-
-                pass
                 my_stack.msg = "invalid 443 listener; forward to target group"
+
         else:
             my_stack.msg = "invalid port on listener"
-            #return '{"msg":"invalid port on listener"}'
 
     response = client.describe_target_health(
         TargetGroupArn = my_stack.target_group
     )
+    print("targetInfo:")
+    print(response)
     targets = []
     for target in response['TargetHealthDescriptions']:
         targets.append(target['Target']['Id'])
@@ -281,6 +392,9 @@ def getASGinfo(my_stack):
         launchconfig.append(instance['LaunchConfigurationName'])
     if (len(set(asg))!=1):
         my_stack.msg = "you cannot use 2 ASGs!"
+    if (len(set(launchconfig))!=1):
+        my_stack.msg = "you cannot use 2 launchconfig!"
+
     my_stack.ASG = asg[0]
     my_stack.launchconfig = launchconfig[0]
 
@@ -303,18 +417,28 @@ def getASGinfo(my_stack):
 def updateLastChecked(my_stack):
     client = boto3.client('dynamodb')
     print(my_stack.id)
-    response = client.update_item(
-                    TableName = 'sourceshare',
+    if my_stack.msg is 'pass':
+        response = client.update_item(
+                    TableName = 'sourcereports',
                     Key = {'id':{'S':my_stack.id},'type':{'S':'stack'}},
-                    UpdateExpression='SET lastchecked = :val1, pass = :val2',
+                    UpdateExpression='SET lastchecked = :val1, msg = :val2',
                     ExpressionAttributeValues={
                         ':val1': {'N':str(time.time())},
                         ':val2': {'S':my_stack.msg}
                     }
                 )
+    else:
+        response = client.update_item(
+                TableName = 'sourcereports',
+                Key = {'id':{'S':my_stack.id},'type':{'S':'stack'}},
+                UpdateExpression='SET msg = :val1',
+                ExpressionAttributeValues={
+                    ':val1': {'S':my_stack.msg}
+                }
+            )
     if my_stack.new is 1:
         response = client.update_item(
-                    TableName = 'sourceshare',
+                    TableName = 'sourcereports',
                     Key = {'id':{'S':my_stack.id},'type':{'S':'stack'}},
                     UpdateExpression='SET branch = :val1, repo = :val2, #source = :val3',
                     ExpressionAttributeValues={
@@ -324,24 +448,18 @@ def updateLastChecked(my_stack):
                     },
                     ExpressionAttributeNames={"#source": "source"}
                 )
-        print('new repo added')
+    if my_stack.flag is 1:
+        response = client.update_item(
+                    TableName = 'sourcereports',
+                    Key = {'id':{'S':my_stack.id},'type':{'S':'stack'}},
+                    UpdateExpression='SET flag = :val1',
+                    ExpressionAttributeValues={
+                        ':val1': {'S':str(my_stack.flag)}
+                    }
+                )
     print (response)
 
-def verifyBucketRedirect(my_stack):
-    print(my_stack.redirect_bucket)
-    bucketname = my_stack.redirect_bucket
-    bucketname = bucketname.split('.s3-website')[0]
-    print(bucketname)
 
-    client = boto3.client('s3',
-        aws_access_key_id=my_stack.credentials['ACCESS_KEY'],
-        aws_secret_access_key=my_stack.credentials['SECRET_KEY'],
-        aws_session_token=my_stack.credentials['SESSION_TOKEN']
-    )
-    response = client.get_bucket_website(
-        Bucket=bucketname
-    )
-    print ('bucket info')
-    print (response)
-    if response['RedirectAllRequestsTo']['HostName'] != my_stack.url:
-        my_stack.msg = 'Second url is not redirected to primary url'
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
